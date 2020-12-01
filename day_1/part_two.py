8030cd1fcd539c8b12a4b34b87907d07ec13128c
Gabe Correa <gabriel.correa@gmail.com>
2020-12-01 12:42:20 -0800

    part 2 complete

--- day_1/part_two.py
+++ day_1/part_two.py
@@ -1,16 +1,19 @@
+from functools import reduce
 
 file = open('aoc2020/day_1/input.txt', 'r')
 
 expenses = list(map(lambda expense: int(expense), file.read().split('\n')))
 expenses.sort()
 
-left_index, right_index = 0, -1
-sum = 0
-while sum != 2020:
-  sum = expenses[left_index] + expenses[right_index]
-  if sum > 2020:
-    right_index -= 1
-  elif sum < 2020:
-    left_index += 1
-
-print(expenses[left_index] * expenses[right_index])
+for x in expenses:
+  for y in expenses[1::]:
+    for z in expenses[2::]:
+      if x + y + z == 2020:
+        print(x * y * z)
+        break
+    else:
+      continue
+    break
+  else:
+    continue
+  break
