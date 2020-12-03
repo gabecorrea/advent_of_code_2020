7a8f5733bc1c8b4d6dad067242d94c5c80594d02
Gabe Correa <gabriel.correa@gmail.com>
2020-12-03 11:48:44 -0800

    part 2 complete

---
+++ day_3/part_two.py
@@ -0,0 +1,31 @@
+from functools import reduce
+
+grid = open('aoc2020/day_3/input.txt', 'r').read().split('\n')
+pattern_boundary = len(grid[0]) - 1
+bottom_row = len(grid) - 1
+
+def damn_the_trees_full_speed_ahead(slope):
+  x, y = slope[0], slope[1]
+  num_trees = 0
+  while(y <= bottom_row):
+    if x in range(pattern_boundary + 1, pattern_boundary + slope[0] + 1):
+      # rollover
+      x -= pattern_boundary + 1
+    
+    if grid[y][x] == '#':
+      num_trees += 1
+
+    x += slope[0]
+    y += slope[1]
+  print(num_trees)
+  return num_trees
+
+test_slopes = [
+  (1, 1),
+  (3, 1),
+  (5, 1),
+  (7, 1),
+  (1, 2),
+]
+
+print(reduce( lambda x, y: x * y, list(map(lambda slope: damn_the_trees_full_speed_ahead(slope), test_slopes))))
