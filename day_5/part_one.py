3d5100e8be399b807c3a1412bb1212f12afd2a2d
Gabe Correa <gabriel.correa@gmail.com>
2020-12-05 16:10:46 -0800

    remove comments

--- day_5/part_one.py
+++ day_5/part_one.py
@@ -3,7 +3,7 @@ boarding_codes = open('aoc2020/day_5/input.txt').read().split('\n')
 def find_seat_position(boarding_code, possible_values):
   code = boarding_code.pop(0)
   position = 0
-  #if code in ['F', 'B']:
+
   if len(possible_values) == 2:
     return possible_values[0] if code in ['F','L'] else possible_values[1]
   
@@ -12,16 +12,6 @@ def find_seat_position(boarding_code, possible_values):
     position = find_seat_position(boarding_code, possible_values[:mid])
   else:
     position = find_seat_position(boarding_code, possible_values[mid:])
-
-  # if code in ['L', 'R']:
-  #   if len(possible_values) == 2:
-  #     return possible_values[0] if code == 'L' else possible_values[1]
-    
-  #   mid = int(len(possible_values) / 2)
-  #   if code == 'L':
-  #     position = find_seat_position(boarding_code, possible_values[:mid])
-  #   else:
-  #     position = find_seat_position(boarding_code, possible_values[mid:])
 
   return position
 
@@ -32,7 +22,5 @@ seat_rows = [
 seat_cols = [
   find_seat_position(list(x)[7:], list(range(0,8))) for x in boarding_codes
 ]
-# print(seat_rows)
-# print('----')
-# print(seat_cols)
+
 print(max([seat_rows[i] * 8 + seat_cols[i] for i in range(len(seat_rows))]))
