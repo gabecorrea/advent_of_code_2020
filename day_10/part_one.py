2e33011301255e1177cb9e55079e228b452b6d13
Gabe Correa <gabriel.correa@gmail.com>
2020-12-10 10:51:32 -0800

    part one complete

---
+++ day_10/part_one.py
@@ -0,0 +1,9 @@
+adapter_charges = [int(charge) for charge in open('aoc2020/day_10/input.txt').read().splitlines()]
+adapter_charges.sort()
+print(len(adapter_charges))
+differences = []
+for index in range(len(adapter_charges) - 1):
+  differences.append(adapter_charges[index+1] - adapter_charges[index])
+
+# also account for charge from initial charger and device adapter
+print((1 + differences.count(1)) * (differences.count(3) + 1))
